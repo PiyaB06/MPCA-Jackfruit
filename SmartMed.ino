c519a8bc56f752c0246eae7b5dc055679c355c8d
PiyaB06 <145908698+PiyaB06@users.noreply.github.com>
2026-04-21 15:11:02 +0530

    Enhance alarm functionality for multiple alarms
    Refactor alarm handling to support multiple alarms with specific days. Update button handling logic for setting alarms.

--- SmartMed.ino
+++ SmartMed.ino
@@ -5,23 +5,31 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 RTC_DS3231 rtc;
 
+#define MAX_ALARMS 3
+
 // PINS
 int buzzer = 8;
 int led = 2;
-int btnMode = 7;   // D7
-int btnAction = 9; // D9
+int btnMode = 7;
+int btnAction = 9;
 
-// MED TIME
-int med_hour = 0;
-int med_min = 00;
+// ALARM DATA
+int med_hour[MAX_ALARMS] = {8, 14, 20};
+int med_min[MAX_ALARMS]  = {0, 0, 0};
+int med_day[MAX_ALARMS]  = {1, 1, 1};
+
+// DAY NAMES
+const char* days[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 
 // STATES
 bool alarmActive = false;
-bool alarmTriggered = false;
+bool alarmTriggered[MAX_ALARMS] = {false, false, false};
 bool settingMode = false;
-bool settingHour = true;
 
-// BUTTON TIMING
+int currentAlarm = 0;
+int settingField = 0;
+
+// BUTTON
 unsigned long pressStart = 0;
 bool longPressHandled = false;
 
@@ -35,79 +43,84 @@ void setup() {
   pinMode(btnAction, INPUT_PULLUP);
 
   rtc.begin();
-
 }
 
 void loop() {
   DateTime now = rtc.now();
 
-  handleButtons(now);
+  handleButtons();
   handleAlarm(now);
   display(now);
 
   delay(200);
 }
 
-void handleButtons(DateTime now) {
-  // LONG PRESS DETECTION (D7)
+// ---------------- BUTTON LOGIC ----------------
+void handleButtons() {
   if (digitalRead(btnMode) == LOW) {
     if (pressStart == 0) pressStart = millis();
 
     if (millis() - pressStart > 800 && !longPressHandled) {
       longPressHandled = true;
-
-      if (!settingMode) {
-        settingMode = true;   // ENTER setting
-      } else {
-        settingMode = false;  // EXIT setting
-      }
+      settingMode = !settingMode;
     }
   } else {
-    // Button released
     if (pressStart != 0 && !longPressHandled) {
-      // SHORT PRESS → switch field
       if (settingMode) {
-        settingHour = !settingHour;
+        settingField++;
+        if (settingField > 2) {
+          settingField = 0;
+          currentAlarm = (currentAlarm + 1) % MAX_ALARMS;
+        }
       }
     }
-
     pressStart = 0;
     longPressHandled = false;
   }
 
-  // D9 BUTTON
   if (digitalRead(btnAction) == LOW) {
     if (settingMode) {
-      // INCREMENT TIME
-      if (settingHour) {
-        med_hour = (med_hour + 1) % 24;
-      } else {
-        med_min = (med_min + 5) % 60;
-      }
+      if (settingField == 0)
+        med_hour[currentAlarm] = (med_hour[currentAlarm] + 1) % 24;
+
+      else if (settingField == 1)
+        med_min[currentAlarm] = (med_min[currentAlarm] + 5) % 60;
+
+      else if (settingField == 2)
+        med_day[currentAlarm] = (med_day[currentAlarm] + 1) % 7;
+
       delay(200);
-    } else if (alarmActive) {
-      // STOP ALARM
+    }
+    else if (alarmActive) {
       alarmActive = false;
       digitalWrite(buzzer, LOW);
       digitalWrite(led, LOW);
 
-      lcd.setCursor(0, 1);
+      lcd.setCursor(0,1);
       lcd.print("                ");
       delay(300);
     }
   }
 }
 
+// ---------------- ALARM LOGIC ----------------
 void handleAlarm(DateTime now) {
-  if (!settingMode) {
-    // TRIGGER ONCE
-    if (now.hour() == med_hour && now.minute() == med_min && !alarmTriggered) {
-      alarmActive = true;
-      alarmTriggered = true;
-    }
+  int today = now.dayOfTheWeek();
 
-    if (now.minute() != med_min) {
-      alarmTriggered = false;
+  for (int i = 0; i < MAX_ALARMS; i++) {
+    if (!settingMode) {
+      if (now.hour() == med_hour[i] &&
+          now.minute() == med_min[i] &&
+          today == med_day[i] &&
+          !alarmTriggered[i]) {
+
+        alarmActive = true;
+        alarmTriggered[i] = true;
+      }
+
+      if (now.minute() != med_min[i]) {
+        alarmTriggered[i] = false;
+      }
     }
   }
 
@@ -117,8 +130,9 @@ void handleAlarm(DateTime now) {
   }
 }
 
+// ---------------- DISPLAY ----------------
 void display(DateTime now) {
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);
 
   if (!settingMode) {
     lcd.print("Time: ");
@@ -128,36 +142,53 @@ void display(DateTime now) {
     lcd.print(":");
     print2(now.second());
 
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
 
     if (alarmActive) {
-      lcd.print("TAKE MEDICINE! ");
+      lcd.print("TAKE MEDICINE!");
     } else {
-      lcd.print("Next: ");
-      print2(med_hour);
+      lcd.print("A");
+      lcd.print(currentAlarm+1);
+      lcd.print(" ");
+
+      print2(med_hour[currentAlarm]);
       lcd.print(":");
-      print2(med_min);
-      lcd.print("       ");
+      print2(med_min[currentAlarm]);
+
+      lcd.print(" ");
+      lcd.print(days[med_day[currentAlarm]]);
+      lcd.print(" ");
     }
-  } else {
-    lcd.print("Set Time:");
+  } 
+  else {
+    lcd.print("Set A");
+    lcd.print(currentAlarm+1);
+    lcd.print("      ");
 
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
 
-    if (settingHour) lcd.print("H:");
-    else lcd.print(" ");
+    if (settingField == 0) lcd.print("H:");
+    else lcd.print("  ");
 
-    print2(med_hour);
+    print2(med_hour[currentAlarm]);
     lcd.print(":");
 
-    if (!settingHour) lcd.print("M:");
+    if (settingField == 1) lcd.print("M:");
+    else lcd.print("  ");
+
+    print2(med_min[currentAlarm]);
+
+    lcd.print(" ");
+
+    if (settingField == 2) lcd.print("*");
     else lcd.print(" ");
 
-    print2(med_min);
-    lcd.print("   ");
+    lcd.print(days[med_day[currentAlarm]]);
+    lcd.print(" ");
   }
 }
 
+// ---------------- HELPER ----------------
 void print2(int val) {
   if (val < 10) lcd.print("0");
   lcd.print(val);
